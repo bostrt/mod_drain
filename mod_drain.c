de1edbaf6d094d434d7c2c11d7a6ae3aa17e9f55
Robert Bost <bostrt@gmail.com>
2017-09-01 20:59:19 -0400

    sssshhh compiler

--- mod_drain.c
+++ mod_drain.c
@@ -15,6 +15,8 @@
 #include <httpd.h>
 #include <http_config.h>
 #include <http_log.h>
+#include <http_protocol.h>
+#include <util_script.h>
 #include <stdlib.h>
 
 static apr_pool_t *bad_pool = NULL;
